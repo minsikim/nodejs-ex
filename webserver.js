d1a0b2fdad0327086858add96be445a531cfeb8f
minsikim <minsiua@gmail.com>
2018-02-09 12:59:19 +0900

    0.0.2

--- webserver.js
+++ webserver.js
@@ -1,8 +1,9 @@
+//require: nodejs에 구비되어 있는 (http모듈)을 가져와서 사용한다.
 const http = require('http');
-
+// 상수 선언
 const hostname = '127.0.0.1';
 const port = 1337;
-
+//새로운 서버 구성
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
