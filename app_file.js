74b9a88477202f9495dd234495ff4834fd7b7daf
minsikim <minsiua@gmail.com>
2018-02-10 15:39:17 +0900

    1.0.7 multer function

--- app_file.js
+++ app_file.js
@@ -3,7 +3,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 //파일시스템 제어용 모듈 로드
 var fs = require('fs');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: _storage})
 var app = express();
+app.locals.pretty = true;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -13,6 +24,18 @@ app.set('views', './views_file');
 app.listen(3000, function(){
   console.log('connected 3000 port!');
 })
+
+//업로드 페이지 라우트
+app.get('/upload', function(req, res){
+  res.render('upload')
+})
+
+app.post('/upload', upload.single('userfile'), function(req, res){
+  // fs.readFile
+  console.log(req.file);
+  res.send('업로드 성공: '+req.file.originalname)
+})
+
 
 app.get('/topic/new', function(req, res){
   fs.readdir('data', function(err, files){
