3e64e5deb560fca4edc9f953e9826fbef498bc2a
minsikim <minsiua@gmail.com>
2018-02-10 09:54:26 +0900

    1.0.3 w/jade

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ var app = express();
 //스태틱한 자료 사용,
 //public폴더를 사용 localhost 디렉토리에 바로 있는 것 처럼 사용
 app.use(express.static('public'));
+//사용할 템플릿 엔진을 지정해줌, 명령어 : 정해진 형식임
+app.set('view engine', 'jade');
+//템플릿(.jade)이 있는 폴더를 지정, 지정하지 않아도 views로 들어감
+app.set('views', './views');
 //param1 = 포트번호
+app.get('/template', function(req, res){
+  //뷰엔진(지정된템플릿엔진)으로 해석할 파일명
+  //views 폴더에 temp.jade 파일이 있어야함
+  //{중괄호에 json 형식을 주면  jade가 읽을수 있는
+  //variable를 전달할 수 있음 jade, html로}
+  res.render('temp', {time:Date()})
+});
 app.listen(3000, function(){
   console.log('connected 3000 port!');
 })
@@ -47,6 +58,7 @@ app.get('/dynamic', function(req, res){
 //다이나믹하게 웹페이지를 생성
 app.get('/dynamic2', function(req, res){
   var li = '';
+  var time = Date();
   for(var i = 0; i<5; i++){
     li += '<li>Cool stuff</li>';
   }
@@ -61,6 +73,7 @@ app.get('/dynamic2', function(req, res){
         <ul>
         ${li}
         </ul>
+        ${time}
     </body>
   </html>
 `;
