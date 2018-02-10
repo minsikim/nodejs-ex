590060ef1348da30bd27246e0b4d1bcf1079372d
minsikim <minsiua@gmail.com>
2018-02-10 12:32:32 +0900

    1.0.4 form post

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
 var app = express();
+//post 방식의 입력값을 파싱하기 위한 모듈 불러오고 사용
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //스태틱한 자료 사용,
 //public폴더를 사용 localhost 디렉토리에 바로 있는 것 처럼 사용
 app.use(express.static('public'));
+
 //사용할 템플릿 엔진을 지정해줌, 명령어 : 정해진 형식임
 app.set('view engine', 'jade');
 //템플릿(.jade)이 있는 폴더를 지정, 지정하지 않아도 views로 들어감
@@ -78,5 +83,31 @@ app.get('/dynamic2', function(req, res){
   </html>
 `;
 
-  res.send(dy);
 })
+//쿼리스티링(id=1)
+//예: localhost:3000/index => /index.jade => /index.html
+//만약 localhost:3000/index?id=1 => ?
+//이런 변형가능한 다이나믹한 페이지 생성을 위한 것이 쿼리스트링
+//아래 는 루트/topic?id=1 하면 req에 ?이후가 저장되고
+//&로 여러개의 req인자를 전달
+app.get('/topic', function(req, res){
+  res.send(req.query.id+req.query.name);
+});
+//쿼리스트링에 불포함하면서 라우팅하려면 GET 말고 POST 식으로
+app.get('/form', function(req, res){
+  res.render('form');
+});
+//form에서 get방식일경우(method='get')
+app.get('/formto', function(req, res){
+  // res.send('get!');
+  var name = req.query.title;
+  var article = req.query.article;
+  res.send(name + ','+article);
+});
+//form에서 post방식일경우(method='post')
+app.post('/formto', function(req, res){
+  // res.send('post!');
+  var name = req.body.title;
+  var article = req.body.article;
+  res.send(name + ','+article);
+});
