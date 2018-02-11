71e2737ede24b21da1ee5a0485bcb35864779626
minsikim <minsiua@gmail.com>
2018-02-12 02:05:54 +0900

    1.1.2 cookie parser

--- app_cookie.js
+++ app_cookie.js
@@ -3,6 +3,26 @@ var cookieParser = require('cookie-parser');
 var app = express();
 app.use(cookieParser());
 
+var products = {
+  1:{title:'The history of web 1'},
+  2:{title:'The next web'},
+}
+
+app.get('/products/', function(req, res){
+  var output = '';
+  for(var name in products){
+    output += `<li>
+    <a href="/cart/${name}">${products[name].title}
+    </li>`;
+  }
+  res.send(`<h1>Products</h1><ul>${output}<ul></br><a href="/cart/">Cart</a>`);
+});
+
+app.get('/cart/:id', function(req, res){
+
+  res.send
+}
+
 app.get('/cookie/', function(req, res){
   if(req.cookies.count){
     var count = req.cookies.count
