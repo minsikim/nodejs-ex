9dbe6b5a5c58682a718717a5ccc7a581b4f966bc
minsikim <minsiua@gmail.com>
2018-02-11 09:45:40 +0900

    1.1.1 cookie simple app

--- app_cookie.js
+++ app_cookie.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
+
+app.get('/cookie/', function(req, res){
+  if(req.cookies.count){
+    var count = req.cookies.count
+  }
+  else var count = 0;
+  count++;
+  res.cookie('count', count);
+  console.log('log : '+req.cookies.count);
+  res.send('count : '+count);
+})
 
 app.listen(3000, function(){
   console.log('app_cookie.js, connected 3000 port!');
-})
-
-app.get('cookie/', function(req, res){
-  var count = 0;
-  res.send('count'+count);
 })
